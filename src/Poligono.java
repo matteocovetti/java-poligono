d2467e4d4e379f12bdcfd15521036c2de8ba34a4
matteocovetti <covetti.matteo@gmail.com>
2022-06-28 15:27:12 +0200

    first commit

---
+++ src/Poligono.java
@@ -0,0 +1,11 @@
+
+public interface Poligono {
+	/*
+	 * Creare un’interfaccia Poligono con due metodi: calcolaPerimetro() e
+	 * calcolaArea(). Creare almeno due classi concrete che rappresentano dei
+	 * poligoni e che implementano l’interfaccia. Ogni classe può avere un insieme
+	 * di propri attributi, costruttori e anche altri metodi oltre a quelli
+	 * dell’interfaccia. Creare una classe Main di test in cui generare delle
+	 * istanze dei poligoni e testare i metodi di calcolo del perimetro e dell’area.
+	 */
+}
