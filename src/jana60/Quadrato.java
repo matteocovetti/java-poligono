24dc3ce6d9462e2ae341dc1799bec49b505ec8db
matteocovetti <covetti.matteo@gmail.com>
2022-06-28 15:39:42 +0200

    add quadrato

---
+++ src/jana60/Quadrato.java
@@ -0,0 +1,41 @@
+package jana60;
+
+public class Quadrato implements Poligono {
+	// attributi
+	private double lato;
+
+	// costruttore
+	public Quadrato(double lato) throws IllegalArgumentException {
+		super();
+		validaLato(lato);
+		this.lato = lato;
+	}
+
+	// getter e setter
+	public double getLato() {
+		return lato;
+	}
+
+	public void setLato(double lato) throws IllegalArgumentException {
+		validaLato(lato);
+		this.lato = lato;
+	}
+
+	// metodi
+	@Override
+	public double calcolaPerimetro() {
+		return lato * 4;
+	}
+
+	@Override
+	public double calcolaArea() {
+		return lato * lato;
+	}
+
+	// controlli
+	public void validaLato(double lato) throws IllegalArgumentException {
+		if (lato <= 0) {
+			throw new IllegalArgumentException("Il valore del lato non può essere uguale o minore a zero");
+		}
+	}
+}
